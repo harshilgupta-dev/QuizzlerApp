b1fac0f227484cfe0097454969c36d163064a877
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-07 14:22:33 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatefulWidget {
+  const Quizzler({Key key}) : super(key: key);
+
+  @override
+  _QuizzlerState createState() => _QuizzlerState();
+}
+
+class _QuizzlerState extends State<Quizzler> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[800],
+      ),
+    );
+  }
+}
