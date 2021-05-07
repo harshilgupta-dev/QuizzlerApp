a9a8ae24741db47cbe8fcc048a1c69da054f70b0
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-07 16:15:07 +0530

    create List<Question>

--- lib/main1.dart
+++ lib/main1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'defination/Question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,10 +27,10 @@ class QuizPage extends StatefulWidget {
 }
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKepper=[];
-  List<String> questions=[
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionsBank=[
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q:'A slug\'s blood is green.', a: true)
   ];
   int questionNumber=0;
   @override
